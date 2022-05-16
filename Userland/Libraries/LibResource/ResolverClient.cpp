1b372a94d1ca0ce8bfcc7d5bbb2d16e4ce550336
MacDue <macdue@dueutil.tech>
2022-05-16 01:08:51 +0100

    fin libresouce

--- Userland/Libraries/LibResource/ResolverClient.cpp
+++ Userland/Libraries/LibResource/ResolverClient.cpp
@@ -4,14 +4,33 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-#include <ResolverClient.h>
+#include <LibResource/ResolverClient.h>
 
 namespace Resource {
 
-static ResolverClient& ResolverClient::the()
+ResolverClient& ResolverClient::the()
 {
     static auto connection = ResolverClient::try_create().release_value_but_fixme_should_propagate_errors();
     return connection;
+}
+
+i32 ResolverClient::add_path_watcher(PathWatcher& path_watcher)
+{
+    auto id = m_id_allocator.allocate();
+    m_path_watchers.set(id, &path_watcher);
+    return id;
+}
+
+void ResolverClient::remove_path_watcher(i32 id)
+{
+    m_path_watchers.remove(id);
+    m_id_allocator.deallocate(id);
+}
+
+void ResolverClient::resource_paths_updated()
+{
+    for (auto [_, path_watcher] : m_path_watchers)
+        path_watcher->notify();
 }
 
 }
--- Userland/Libraries/LibResource/ResolverClient.h
+++ Userland/Libraries/LibResource/ResolverClient.h
@@ -7,35 +7,72 @@
 #pragma once
 
 #include <AK/Function.h>
+#include <AK/HashMap.h>
+#include <AK/IDAllocator.h>
+#include <LibGfx/Bitmap.h>
 #include <LibIPC/ConnectionToServer.h>
 #include <ResourceServer/ResourceClientEndpoint.h>
 #include <ResourceServer/ResourceServerEndpoint.h>
 
 namespace Resource {
 
-using namespace ResourceServer;
+class PathWatcher;
 
 class ResolverClient final
     : public IPC::ConnectionToServer<ResourceClientEndpoint, ResourceServerEndpoint>
-    , public LaunchClientEndpoint {
+    , public ResourceClientEndpoint {
     IPC_CLIENT_CONNECTION(ResolverClient, "/tmp/portal/resource")
+public:
+    friend PathWatcher;
+    static ResolverClient& the();
+
 private:
+    i32 add_path_watcher(PathWatcher& path_watchers);
+    void remove_path_watcher(i32 id);
+
     ResolverClient(NonnullOwnPtr<Core::Stream::LocalSocket> socket)
         : IPC::ConnectionToServer<ResourceClientEndpoint, ResourceServerEndpoint>(*this, move(socket))
-        , m_event_target { event_target }
     {
     }
 
-    virtual void resource_paths_updated() override
+    virtual void resource_paths_updated() override;
+
+    ErrorOr<NonnullRefPtr<Gfx::Bitmap>> try_resolve_and_load_bitmap(String const& path)
+    {
+        return Gfx::Bitmap::try_load_from_file(resolve(path));
+    }
+
+    IDAllocator m_id_allocator;
+    HashMap<i32, PathWatcher*> m_path_watchers;
+};
+
+class PathWatcher final {
+public:
+    using PathsUpdatedCallback = Function<void()>;
+
+    PathWatcher()
+    {
+        m_id = ResolverClient::the().add_path_watcher(*this);
+    }
+
+    ~PathWatcher()
+    {
+        ResolverClient::the().remove_path_watcher(m_id);
+    }
+
+    void notify()
     {
         if (on_resource_paths_updated)
             on_resource_paths_updated();
     }
 
-public:
-    static ResolverClient& the();
+    PathsUpdatedCallback on_resource_paths_updated;
 
-    Function<void()> on_resource_paths_updated;
+    AK_MAKE_NONCOPYABLE(PathWatcher);
+    AK_MAKE_NONMOVABLE(PathWatcher);
+
+private:
+    i32 m_id;
 };
 
 }
