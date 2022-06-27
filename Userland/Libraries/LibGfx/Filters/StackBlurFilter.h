63603571afe1566233129ebd1b2d154701f1d163
MacDue <macdue@dueutil.tech>
2022-06-27 19:14:17 +0100

    wip

---
+++ Userland/Libraries/LibGfx/Filters/StackBlurFilter.h
@@ -0,0 +1,26 @@
+#pragma once
+
+/*
+ * Copyright (c) 2022, MacDue <macdue@dueutil.tech>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <LibGfx/Bitmap.h>
+
+namespace Gfx {
+
+class StackBlurFilter {
+public:
+    StackBlurFilter(Bitmap& bitmap)
+      : m_bitmap(bitmap) {}
+
+    void process_rgba(size_t radius);
+
+private:
+    Bitmap& m_bitmap;
+};
+
+}
