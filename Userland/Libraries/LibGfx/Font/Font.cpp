865293c0ed6a9e6b2f59e984cf29c6a40669efb0
MacDue <macdue@dueutil.tech>
2023-01-05 00:36:05 +0000

    WIP

--- Userland/Libraries/LibGfx/Font/Font.cpp
+++ Userland/Libraries/LibGfx/Font/Font.cpp
@@ -11,9 +11,9 @@ namespace Gfx {
 GlyphRasterPosition GlyphRasterPosition::get_nearest_fit_for(FloatPoint position)
 {
     constexpr auto subpixel_divisions = GlyphSubpixelOffset::subpixel_divisions();
-    auto compute_offset = [](float pos, int& blit_pos, u8& subpixel_offset) {
-        blit_pos = floor(pos);
-        subpixel_offset = AK::ceil((pos - blit_pos) / (1.0f / subpixel_divisions));
+    auto fit = [](float pos, int& blit_pos, u8& subpixel_offset) {
+        blit_pos = floorf(pos);
+        subpixel_offset = round_to<u8>((pos - blit_pos) / (1.0f / subpixel_divisions));
         if (subpixel_offset >= subpixel_divisions) {
             blit_pos += 1;
             subpixel_offset = 0;
@@ -21,8 +21,8 @@ GlyphRasterPosition GlyphRasterPosition::get_nearest_fit_for(FloatPoint position
     };
     int blit_x, blit_y;
     u8 subpixel_x, subpixel_y;
-    compute_offset(position.x(), blit_x, subpixel_x);
-    compute_offset(position.y(), blit_y, subpixel_y);
+    fit(position.x(), blit_x, subpixel_x);
+    fit(position.y(), blit_y, subpixel_y);
     return GlyphRasterPosition { { blit_x, blit_y }, { subpixel_x, subpixel_y } };
 }
 
