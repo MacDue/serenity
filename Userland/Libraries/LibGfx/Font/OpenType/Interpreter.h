dc2adf9c26ffc1a0d28bef0fab6e817cfc2abe40
MacDue <macdue@dueutil.tech>
2023-01-07 02:55:38 +0000

    aahhhh wip

--- Userland/Libraries/LibGfx/Font/OpenType/Interpreter.h
+++ Userland/Libraries/LibGfx/Font/OpenType/Interpreter.h
@@ -10,7 +10,7 @@
 #include <AK/Span.h>
 #include <LibGfx/Point.h>
 
-namespace OpenType {
+namespace OpenType::Hinting {
 
 using F2Dot14 = FixedPoint<30, u16>;
 using F2Dot30 = FixedPoint<30, u32>;
@@ -177,17 +177,170 @@ using F26Dot6 = FixedPoint<6, u32>;
     __ENUMERATE_OPENTYPE_OPCODES(GETINFO, 0x88)         \
     __ENUMERATE_OPENTYPE_OPCODES(GETVARIATION, 0x91)
 
-struct Interpreter {
-
-    // https://learn.microsoft.com/en-us/typography/opentype/spec/tt_instructions
-    enum class Opcode : u8 {
+// https://learn.microsoft.com/en-us/typography/opentype/spec/tt_instructions
+enum class Opcode : u8 {
 #define __ENUMERATE_OPENTYPE_OPCODES(opcode, value) opcode = value,
-        ENUMERATE_OPENTYPE_OPCODES
+    ENUMERATE_OPENTYPE_OPCODES
 #undef __ENUMERATE_OPENTYPE_OPCODES
-    };
+};
 
-    static StringView opcode_name(Opcode);
-    static void debug_print_instruction_stream(ReadonlyBytes stream);
+StringView opcode_name(Opcode);
+
+struct InstructionHandler;
+
+struct InstructionStream {
+    InstructionStream(InstructionHandler& handler, ReadonlyBytes bytes)
+        : m_handler { handler }
+        , m_bytes(bytes)
+    {
+    }
+
+    void process_next_instruction();
+    bool at_end() const;
+    void jump_to_next(Opcode);
+
+private:
+    Opcode next_opcode() { static_cast<Opcode>(next_byte()); }
+
+    u8 next_byte();
+    ReadonlyBytes take_n_bytes(size_t n);
+
+    InstructionHandler& m_handler;
+    ReadonlyBytes m_bytes;
+    size_t m_byte_index { 0 };
+};
+
+// Callbacks for each logically identical set of opcodes.
+// Basic parsing handled by InstructionStream.
+struct InstructionHandler {
+    virtual void npush_bytes(InstructionStream&, ReadonlyBytes values) { }
+    virtual void npush_words(InstructionStream&, ReadonlyBytes values) { }
+    virtual void push_bytes(InstructionStream&, ReadonlyBytes values) { }
+    virtual void push_words(InstructionStream&, ReadonlyBytes values) { }
+
+    virtual void read_store(InstructionStream&) { }
+    virtual void write_store(InstructionStream&) { }
+
+    virtual void write_control_value_table_px(InstructionStream&) { }
+    virtual void write_control_value_table_fuints(InstructionStream&) { }
+    virtual void read_control_value_table(InstructionStream&) { }
+
+    virtual void set_freedom_and_projection_vectors_to_coordinate_axis(InstructionStream&) { }
+    virtual void set_projection_vector_to_coordinate_axis(InstructionStream&) { }
+    virtual void set_freedom_vector_to_coordinate_axis(InstructionStream&) { }
+    virtual void set_projection_vector_to_line(InstructionStream&) { }
+    virtual void set_freedom_vector_vector_to_line(InstructionStream&) { }
+    virtual void set_freedom_vector_to_projection_vector(InstructionStream&) { }
+    virtual void set_dual_projection_vector_to_line(InstructionStream&) { }
+    virtual void set_projection_vector_from_stack(InstructionStream&) { }
+    virtual void set_freedom_vector_from_stack(InstructionStream&) { }
+    virtual void get_projection_vector(InstructionStream&) { }
+    virtual void get_freedom_vector(InstructionStream&) { }
+    virtual void set_refrence_point(InstructionStream&, int reference_point) { }
+    virtual void set_zone_pointer(InstructionStream&, int zone_pointer) { }
+    virtual void set_zone_pointers(InstructionStream&) { }
+    virtual void round_to_half_grid(InstructionStream&) { }
+    virtual void round_to_grid(InstructionStream&) { }
+    virtual void round_to_double_grid(InstructionStream&) { }
+    virtual void round_down_to_grid(InstructionStream&) { }
+    virtual void round_up_to_grid(InstructionStream&) { }
+    virtual void round_off(InstructionStream&) { }
+    virtual void super_round(InstructionStream&) { }
+    virtual void super_round_45_degrees(InstructionStream&) { }
+    virtual void set_loop_variable(InstructionStream&) { }
+    virtual void set_minimum_distance(InstructionStream&) { }
+    virtual void instruction_execution_control(InstructionStream&) { }
+    virtual void scan_conversion_control(InstructionStream&) { }
+    virtual void scantype(InstructionStream&) { }
+    virtual void set_control_value_table_in(InstructionStream&) { }
+    virtual void set_single_width_cut_in(InstructionStream&) { }
+    virtual void set_single_width(InstructionStream&) { }
+    virtual void set_auto_flip_boolean(InstructionStream&, bool) { }
+    virtual void set_angle_weight(InstructionStream&) { }
+    virtual void set_delta_base(InstructionStream&) { }
+    virtual void set_delta_shift(InstructionStream&) { }
+
+    virtual void get_coordinate_projected_onto_projection_vector(InstructionStream&, bool a) { }
+    virtual void set_coordinate_from_stack_using_projection_and_freedom_vectors(InstructionStream&) { }
+    virtual void measure_distance(InstructionStream&, bool a) { }
+    virtual void measure_pixels_per_em(InstructionStream&) { }
+    virtual void measure_point_size(InstructionStream&) { }
+    virtual void flip_point(InstructionStream&) { }
+    virtual void flip_range_on(InstructionStream&) { }
+    virtual void flip_range_off(InstructionStream&) { }
+    virtual void shift_point_by_last_point(InstructionStream&, bool a) { }
+    virtual void shift_contour_by_last_point(InstructionStream&, bool a) { }
+    virtual void shift_zone_by_last_point(InstructionStream&, bool a) { }
+    virtual void shift_point_by_pixel_amount(InstructionStream&) { }
+    virtual void move_stack_indirect_relative_point(InstructionStream&, bool a) { }
+    virtual void move_direct_absolute_point(InstructionStream&, bool a) { }
+    virtual void move_indirect_absolute_point(InstructionStream&, bool a) { }
+    virtual void move_direct_relative_point(InstructionStream&, bool a, bool b, bool c) { }
+    virtual void move_indirect_relative_point(InstructionStream&, bool a, bool b, bool c) { }
+    virtual void align_relative_point(InstructionStream&) { }
+    virtual void adjust_angle(InstructionStream&) { }
+    virtual void align_points(InstructionStream&) { }
+    virtual void interpolate_point_by_last_relative_stretch(InstructionStream&) { }
+    virtual void untouch_point(InstructionStream&) { }
+    virtual void interpolate_untouched_points_through_outline(InstructionStream&) { }
+
+    virtual void delta_exception_p(InstructionStream&, int p) { }
+    virtual void delta_exception_c(InstructionStream&, int c) { }
+
+    virtual void stack_dup(InstructionStream&) { }
+    virtual void stack_clear(InstructionStream&) { }
+    virtual void stack_swap(InstructionStream&) { }
+    virtual void stack_depth(InstructionStream&) { }
+    virtual void stack_copy_indexed_element(InstructionStream&) { }
+    virtual void stack_move_indexed_element(InstructionStream&) { }
+    virtual void stack_roll_three_elements(InstructionStream&) { }
+
+    virtual void if_test(InstructionStream&) { }
+    virtual void else_case(InstructionStream&) { }
+    virtual void end_if(InstructionStream&) { }
+    virtual void jump_relative_on_true(InstructionStream&) { }
+    virtual void jump(InstructionStream&) { }
+    virtual void jump_relative_on_flase(InstructionStream&) { }
+
+    virtual void less_than(InstructionStream&) { }
+    virtual void less_than_or_equal(InstructionStream&) { }
+    virtual void greater_than(InstructionStream&) { }
+    virtual void greater_than_or_equal(InstructionStream&) { }
+    virtual void equal(InstructionStream&) { }
+    virtual void not_equal(InstructionStream&) { }
+    virtual void odd(InstructionStream&) { }
+    virtual void even(InstructionStream&) { }
+    virtual void logical_and(InstructionStream&) { }
+    virtual void logical_or(InstructionStream&) { }
+    virtual void logical_not(InstructionStream&) { }
+
+    virtual void add(InstructionStream&) { }
+    virtual void subtract(InstructionStream&) { }
+    virtual void divide(InstructionStream&) { }
+    virtual void multiply(InstructionStream&) { }
+    virtual void absolute_value(InstructionStream&) { }
+    virtual void negate(InstructionStream&) { }
+    virtual void floor(InstructionStream&) { }
+    virtual void ceiling(InstructionStream&) { }
+    virtual void max(InstructionStream&) { }
+    virtual void min(InstructionStream&) { }
+
+    virtual void round(InstructionStream&) { }
+    virtual void no_round(InstructionStream&) { }
+
+    virtual void function_definition(InstructionStream&) { }
+    virtual void end_function_definition(InstructionStream&) { }
+    virtual void call_function(InstructionStream&) { }
+    virtual void loop_and_call_function(InstructionStream&) { }
+    virtual void instruction_definition(InstructionStream&) { }
+
+    virtual void debug_call(InstructionStream&) { }
+
+    virtual void get_information(InstructionStream&) { }
+    virtual void get_variation(InstructionStream&) { }
+};
+
+struct Interpreter : InstructionHandler {
 
     // TODO:
     struct Zone { };
