b8694e13bb31f00285756f6763e61b480256f049
MacDue <macdue@dueutil.tech>
2023-01-15 22:36:38 +0000

    LibWeb: Implement the canvas gradients
    This gets:
    
    - CanvasRenderingContext2D.createLinearGradient()
    - CanvasRenderingContext2D.createConicGradient()
    - CanvasRenderingContext2D.createRadialGradient()
    
    Actually working as fill styles for paths and rectangles :^)
    Getting them working for strokes is left as an exercise is
    left as an exercise for the reader.

--- Userland/Libraries/LibWeb/HTML/Canvas/CanvasState.cpp
+++ Userland/Libraries/LibWeb/HTML/Canvas/CanvasState.cpp
@@ -42,7 +42,15 @@ bool CanvasState::is_context_lost()
 
 NonnullRefPtr<Gfx::FillStyle> CanvasState::FillStyle::to_gfx_fill_style()
 {
-    VERIFY_NOT_REACHED();
+    return m_fill_style.visit(
+        [&](Gfx::Color color) -> NonnullRefPtr<Gfx::FillStyle> {
+            if (!m_color_fill_style)
+                m_color_fill_style = Gfx::SolidFillStyle::create(color);
+            return m_color_fill_style.release_nonnull();
+        },
+        [&](JS::Handle<CanvasGradient> gradient) {
+            return gradient->to_gfx_fill_style();
+        });
 }
 
 Gfx::Color CanvasState::FillStyle::to_color_fill_but_fixme_should_accept_any_fill_style() const
