32d5ce39cc82111af513d59ea58440dbf5e9187c
MacDue <macdue@dueutil.tech>
2023-02-01 20:26:33 +0000

    WIP Stub 1

---
+++ Userland/Libraries/LibWeb/HTML/CanvasPattern.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2023, MacDue <macdue@dueutil.tech>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <LibGfx/PaintStyle.h>
+#include <LibWeb/Bindings/PlatformObject.h>
+#include <LibWeb/HTML/Canvas/CanvasDrawImage.h>
+
+namespace Web::HTML {
+
+class CanvasPattern final : public Bindings::PlatformObject {
+    WEB_PLATFORM_OBJECT(CanvasPattern, Bindings::PlatformObject);
+
+public:
+    static WebIDL::ExceptionOr<JS::GCPtr<CanvasPattern>> create(JS::Realm&, CanvasImageSource const& image, StringView repetition)
+    {
+        (void)image;
+        (void)repetition;
+        VERIFY_NOT_REACHED();
+    }
+
+    ~CanvasPattern() = default;
+
+    NonnullRefPtr<Gfx::PaintStyle> to_gfx_paint_style() { VERIFY_NOT_REACHED(); }
+
+private:
+    virtual JS::ThrowCompletionOr<void> initialize(JS::Realm&) override { VERIFY_NOT_REACHED(); }
+};
+
+}
