525686c8bd0033267db66d51722e94a3da5fef42
Luke Wilde <lukew@serenityos.org>
2023-04-05 23:00:08 +0100

    LibWeb: Add XMLDocument stub
    Required by Pixi.js's BitmapFont loader.

---
+++ Userland/Libraries/LibWeb/DOM/XMLDocument.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2023, Luke Wilde <lukew@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <LibWeb/DOM/Document.h>
+
+namespace Web::DOM {
+
+class XMLDocument final : public Document {
+    WEB_PLATFORM_OBJECT(XMLDocument, Document);
+
+public:
+    virtual ~XMLDocument() = default;
+
+private:
+    XMLDocument(JS::Realm& realm, AK::URL const& url)
+        : Document(realm, url)
+    {
+    }
+};
+
+}
