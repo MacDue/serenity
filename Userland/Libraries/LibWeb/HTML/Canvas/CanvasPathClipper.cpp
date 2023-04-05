7ac187c958c0811d5e1b1502a14200ce6d57e4dd
MacDue <macdue@dueutil.tech>
2023-04-05 22:50:59 +0100

    WIP

--- Userland/Libraries/LibWeb/HTML/Canvas/CanvasPathClipper.cpp
+++ Userland/Libraries/LibWeb/HTML/Canvas/CanvasPathClipper.cpp
@@ -9,16 +9,22 @@
 
 namespace Web::HTML {
 
+// FIXME: This pretty naive, we should be able to cut down the allocations here
+// (especially for the paint style which is a bit sad).
+
 ErrorOr<CanvasPathClipper> CanvasPathClipper::create(Gfx::Painter& painter, CanvasClip const& canvas_clip)
 {
     auto bounding_box = enclosing_int_rect(canvas_clip.path.bounding_box());
     Gfx::IntRect actual_save_rect {};
     auto saved_bitmap = TRY(painter.get_region_bitmap(bounding_box, Gfx::BitmapFormat::BGRA8888, actual_save_rect));
+    painter.save();
+    painter.add_clip_rect(bounding_box);
     return CanvasPathClipper(*saved_bitmap, actual_save_rect.location(), canvas_clip);
 }
 
 ErrorOr<void> CanvasPathClipper::apply_clip(Gfx::Painter& painter)
 {
+    painter.restore();
     auto bounding_box = enclosing_int_rect(m_canvas_clip.path.bounding_box());
     Gfx::IntRect actual_save_rect {};
     auto clip_area = TRY(painter.get_region_bitmap(bounding_box, Gfx::BitmapFormat::BGRA8888, actual_save_rect));
