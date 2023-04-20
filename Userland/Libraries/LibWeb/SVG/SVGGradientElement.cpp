1e217b3e257b8ae5384faadfe103bff552503e59
MacDue <macdue@dueutil.tech>
2023-04-20 20:35:40 +0100

    WIP

--- Userland/Libraries/LibWeb/SVG/SVGGradientElement.cpp
+++ Userland/Libraries/LibWeb/SVG/SVGGradientElement.cpp
@@ -11,13 +11,29 @@
  */
 
 #include <LibWeb/Bindings/Intrinsics.h>
+#include <LibWeb/SVG/AttributeNames.h>
 #include <LibWeb/SVG/SVGGradientElement.h>
+#include <LibWeb/SVG/SVGGraphicsElement.h>
 
 namespace Web::SVG {
 
 SVGGradientElement::SVGGradientElement(DOM::Document& document, DOM::QualifiedName qualified_name)
     : SVGElement(document, move(qualified_name))
 {
+}
+
+void SVGGradientElement::parse_attribute(DeprecatedFlyString const& name, DeprecatedString const& value)
+{
+    SVGElement::parse_attribute(name, value);
+    if (name == AttributeNames::gradientUnits) {
+        m_gradient_units = AttributeParser::parse_gradient_units(value);
+    } else if (name == AttributeNames::gradientTransform) {
+        if (auto transform_list = AttributeParser::parse_transform(value); transform_list.has_value()) {
+            m_gradient_transform = transform_from_transform_list(*transform_list);
+        } else {
+            m_gradient_transform = {};
+        }
+    }
 }
 
 JS::ThrowCompletionOr<void> SVGGradientElement::initialize(JS::Realm& realm)
--- Userland/Libraries/LibWeb/SVG/SVGGradientElement.h
+++ Userland/Libraries/LibWeb/SVG/SVGGradientElement.h
@@ -7,6 +7,7 @@
 #pragma once
 
 #include <LibGfx/PaintStyle.h>
+#include <LibWeb/SVG/AttributeParser.h>
 #include <LibWeb/SVG/SVGElement.h>
 
 namespace Web::SVG {
@@ -17,12 +18,28 @@ class SVGGradientElement : public SVGElement {
 public:
     virtual ~SVGGradientElement() override = default;
 
+    virtual void parse_attribute(DeprecatedFlyString const& name, DeprecatedString const& value) override;
+
     virtual Optional<Gfx::PaintStyle const&> to_gfx_paint_style() const = 0;
+
+    GradientUnits gradient_units() const
+    {
+        return m_gradient_units.value_or(GradientUnits::ObjectBoundingBox);
+    }
+
+    Optional<Gfx::AffineTransform> const& gradient_transform() const
+    {
+        return m_gradient_transform;
+    }
 
 protected:
     SVGGradientElement(DOM::Document&, DOM::QualifiedName);
 
     virtual JS::ThrowCompletionOr<void> initialize(JS::Realm&) override;
+
+private:
+    Optional<GradientUnits> m_gradient_units = {};
+    Optional<Gfx::AffineTransform> m_gradient_transform = {};
 };
 
 }
