59cfee80ce30996b80665f752aa3fcabe95f3d9c
MacDue <macdue@dueutil.tech>
2024-03-25 23:14:58 +0000

    WIP

--- Userland/Libraries/LibWeb/Painting/SVGGraphicsPaintable.cpp
+++ Userland/Libraries/LibWeb/Painting/SVGGraphicsPaintable.cpp
@@ -8,6 +8,7 @@
 #include <LibWeb/Layout/SVGClipBox.h>
 #include <LibWeb/Layout/SVGMaskBox.h>
 #include <LibWeb/Painting/CommandExecutorCPU.h>
+#include <LibWeb/Painting/SVGClipPaintable.h>
 #include <LibWeb/Painting/SVGGraphicsPaintable.h>
 #include <LibWeb/Painting/StackingContext.h>
 #include <LibWeb/SVG/SVGMaskElement.h>
@@ -33,12 +34,11 @@ Layout::SVGGraphicsBox const& SVGGraphicsPaintable::layout_box() const
 Optional<CSSPixelRect> SVGGraphicsPaintable::get_masking_area() const
 {
     auto const& graphics_element = verify_cast<SVG::SVGGraphicsElement const>(*dom_node());
-    auto* mask_box = graphics_element.layout_node()->first_child_of_type<Layout::SVGMaskBox>();
     Optional<CSSPixelRect> masking_area = {};
-    if (mask_box)
+    if (auto* mask_box = graphics_element.layout_node()->first_child_of_type<Layout::SVGMaskBox>())
         masking_area = mask_box->dom_node().resolve_masking_area(mask_box->paintable_box()->absolute_border_box_rect());
-    auto* clip_box = graphics_element.layout_node()->first_child_of_type<Layout::SVGClipBox>();
-    if (clip_box) {
+    if (auto* clip_box = graphics_element.layout_node()->first_child_of_type<Layout::SVGClipBox>()) {
+        // This is a bit ad-hoc, but if we have both a mask and a clip-path, intersect the two areas to find the masking area.
         auto clip_area = clip_box->paintable_box()->absolute_border_box_rect();
         if (masking_area.has_value())
             masking_area = masking_area->intersected(clip_area);
@@ -63,8 +63,7 @@ static Gfx::Bitmap::MaskKind mask_type_to_gfx_mask_kind(CSS::MaskType mask_type)
 Optional<Gfx::Bitmap::MaskKind> SVGGraphicsPaintable::get_mask_type() const
 {
     auto const& graphics_element = verify_cast<SVG::SVGGraphicsElement const>(*dom_node());
-    auto mask = graphics_element.mask();
-    if (mask)
+    if (auto mask = graphics_element.mask())
         return mask_type_to_gfx_mask_kind(mask->layout_node()->computed_values().mask_type());
     if (graphics_element.clip_path())
         return Gfx::Bitmap::MaskKind::Alpha;
@@ -75,7 +74,7 @@ RefPtr<Gfx::Bitmap> SVGGraphicsPaintable::calculate_mask(PaintContext& context, 
 {
     auto const& graphics_element = verify_cast<SVG::SVGGraphicsElement const>(*dom_node());
     auto mask_rect = context.enclosing_device_rect(masking_area);
-    auto paint_mask_or_clip = [&](PaintableBox const& mask_paintable, bool is_clip_path = false) -> RefPtr<Gfx::Bitmap> {
+    auto paint_mask_or_clip = [&](PaintableBox const& paintable) -> RefPtr<Gfx::Bitmap> {
         auto mask_bitmap_or_error = Gfx::Bitmap::create(Gfx::BitmapFormat::BGRA8888, mask_rect.size().to_type<int>());
         RefPtr<Gfx::Bitmap> mask_bitmap = {};
         if (mask_bitmap_or_error.is_error())
@@ -86,13 +85,12 @@ RefPtr<Gfx::Bitmap> SVGGraphicsPaintable::calculate_mask(PaintContext& context, 
         recording_painter.translate(-mask_rect.location().to_type<int>());
         auto paint_context = context.clone(recording_painter);
         paint_context.set_svg_transform(graphics_element.get_transform());
-        paint_context.set_draw_svg_geometry_for_clip_path(is_clip_path);
-        StackingContext::paint_node_as_stacking_context(mask_paintable, paint_context);
+        paint_context.set_draw_svg_geometry_for_clip_path(is<SVGClipPaintable>(paintable));
+        StackingContext::paint_node_as_stacking_context(paintable, paint_context);
         CommandExecutorCPU executor { *mask_bitmap };
         painting_commands.execute(executor);
         return mask_bitmap;
     };
-
     RefPtr<Gfx::Bitmap> mask_bitmap = {};
     if (auto* mask_box = graphics_element.layout_node()->first_child_of_type<Layout::SVGMaskBox>()) {
         auto& mask_paintable = static_cast<PaintableBox const&>(*mask_box->paintable());
@@ -100,7 +98,8 @@ RefPtr<Gfx::Bitmap> SVGGraphicsPaintable::calculate_mask(PaintContext& context, 
     }
     if (auto* clip_box = graphics_element.layout_node()->first_child_of_type<Layout::SVGClipBox>()) {
         auto& clip_paintable = static_cast<PaintableBox const&>(*clip_box->paintable());
-        auto clip_bitmap = paint_mask_or_clip(clip_paintable, true);
+        auto clip_bitmap = paint_mask_or_clip(clip_paintable);
+        // Combine the clip-path with the mask (if present).
         if (mask_bitmap && clip_bitmap)
             mask_bitmap->apply_mask(*clip_bitmap, Gfx::Bitmap::MaskKind::Alpha);
         if (!mask_bitmap)
